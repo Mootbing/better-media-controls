eb81b7225aa2d197304a588fd6ed4560e229e698
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-06-16 16:53:02 -0400

    works

--- App.js
+++ App.js
@@ -1,41 +1,52 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { rtdb } from './src/firebase_auth';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { ios, web } from './ids';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-
-  const [count, setCount] = useState(0);
-  const [offline, setOffline] = useState(false);
-
-  useEffect(() => {
-    rtdb.ref("count").on("value", (snapshot) => {
-      setCount(snapshot.val());
-    })
-  }, [])
-  
-  useEffect(() => {
-    rtdb.ref("count").set(count);
-  }, [count])
+  const [accessToken, setAccessToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: web,
+    iosClientId: ios,
+  });
 
   useEffect(() => {
-    if (offline) {
-      rtdb.goOffline();
-    } else {
-      rtdb.goOnline();
+    if (response?.type === 'success') {
+      setAccessToken(response.authentication.accessToken);
+      accessToken && fetchUserInfo();
     }
-  }, [offline])
-  
-  return (
-    <View style={styles.container}>
-      <View style={{flexDirection: "row"}}>
-        <Button title="+" onPress={() => setCount(count + 1)} />
-        <Text style={{marginLeft: 25, marginRight: 25, color: "white", fontSize: 30}}>{count}</Text>
-        <Button title="-" onPress={() => setCount(count - 1)} />
+  }, [response, accessToken])
+
+  async function fetchUserInfo() {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+    let userInfo = await response.json();
+    setUser(userInfo);
+  }
+
+  const ShowUserInfo = () => {
+    if (user) {
+      return <View>
+        <Text style={{ color: '#fff' }}>Welcome {user.name}</Text>
+        <Image source={{ uri: user.picture }} style={{ width: 100, height: 100 }} />
       </View>
-      <Button title={offline ? "go online (offline rn)" : "go offline (online rn)"} onPress={() => setOffline(!offline)} />
-    </View>
-  );
+    }
+    return null;
+  }
+  
+  return <View style={{display: "flex", width: "100%", height: "100%", justifyContent: "center"}}>
+    {user && <ShowUserInfo />}
+    {!user && <TouchableOpacity onPress={() => promptAsync()} style={{ backgroundColor: '#fff', padding: 10, borderRadius: 5 }}><Text>Log In</Text></TouchableOpacity>}
+  </View>
 }
 
 const styles = StyleSheet.create({
